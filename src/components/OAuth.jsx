74aeed0dd1aa5aa4f17fbdbcdf593653c38bbe11
Cole-Sm1th <csmith543565@gmail.com>
2025-03-17 20:26:25 -0700

    Added functionallity for sign in with google button

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,42 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 export default function
 OAuth(){
+    const navigate = useNavigate()
+    async function onGoogleClick() {
+        try {
+            const auth = getAuth()
+            const provider = new GoogleAuthProvider()
+            const result = await signInWithPopup(auth, provider)
+            const user = result.user
+            
+            // Check if user already exists
+
+            const docRef = doc(db, "users", user.uid)
+            const docSnap = await getDoc(docRef)
+
+            if(!docSnap.exists()){
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                })
+            }
+
+            // Go to home screen
+            navigate("/")
+
+        } catch (error) {
+            toast.error("Could not authorize with Google")
+            
+        }
+    }
     return (
-        <button className="flex
+        <button type="button" onClick={onGoogleClick}className="flex
         items-center
         justify-center
         w-full

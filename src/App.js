ea95be6b5669faf85703bc6a631b3d036513b5e6
Seeodoubleyou <ymyman2004@gmail.com>
2025-03-19 23:40:07 -0700

    added private route functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ForgotPassword from './Pages/ForgotPassword';
 import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/offers" element={<Offers />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
+        <Route path = "/profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />}/>
+        </Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/forgot-password" element={<ForgotPassword />}></Route>
--- src/Pages/Offers.jsx
+++ src/Pages/Offers.jsx
@@ -1,7 +1,7 @@
 export default function Offers() {
     return(
         <div>
-          Offers  
+          Offers
         </div>
     )
 }
